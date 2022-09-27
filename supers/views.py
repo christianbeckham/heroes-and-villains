20913e1030e4993f93db31d6578049ef6b847b5b
Christian Beckham <urenacb22@gmail.com>
2022-09-27 15:20:29 -0500

    feat: added functionality to retrieve all supers and super by id, added API endpoints in project and supers app urls.py files and made migration for Super model class.

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Super
+from .serializers import SuperSerializer
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def supers_list(request):
+    queryset = Super.objects.all()
+    serializer = SuperSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def supers_detail(request, pk):
+    supers = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(supers)
+        return Response(serializer.data, status=status.HTTP_200_OK)
