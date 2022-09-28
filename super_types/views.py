122d9c2dcb3eaf31a97684cb4e72dedf069fbfa1
Christian Beckham <urenacb22@gmail.com>
2022-09-28 11:11:19 -0500

    feat: added functionality to get all SuperType instances with a GET request and updated the urls.py

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import SuperType
+from .serializers import SuperTypeSerializer
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def super_types_list(request):
+    if request.method == 'GET':
+        types = SuperType.objects.all()
+        serializer = SuperTypeSerializer(types, many=True)
+        return Response(serializer.data)
